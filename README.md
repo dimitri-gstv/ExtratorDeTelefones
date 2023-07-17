88f43a919565441a485f3a91aa6134691ec3cf25
dimitri-gstv <dmitri_gstv@proton.me>
2023-07-17 13:20:24 -0300

    ajustes

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 4 - Escolher a opção desejada
 
 ![image](https://github.com/dimitri-gstv/ExtratorDeTelefones/assets/139076055/ec1d3857-f930-467a-adcb-a802b560c5d2)
+
+obs: Utilize pesquisas com dork's para maior resultado..
--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def remove_google_links(links):
 def get_search_results(query):
     url = f"https://www.google.com/search?q={query}"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()
